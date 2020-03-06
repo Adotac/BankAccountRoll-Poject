92fac9bdc9da5621ece9912e4a216ea03cdc4ab8
PeteDaGreat <pete.tiongzon@gmail.com>
2020-03-06 08:40:14 +0800

    bank
    1 finish 3 unfinished #mirotastic

--- BankAcct-Project/BankAcct-Project/BA_header.h
+++ BankAcct-Project/BankAcct-Project/BA_header.h
@@ -4,11 +4,20 @@
 #include <time.h>
 #include <string.h>
 #include <ctype.h>
-
+#define CHARLIMIT 50
+#define ACCTSELT 4 //SIZE OF ARRAY actReply
+#define LTARROW 0x4B 
+#define RTARROW 0x4D 
 #ifndef BA_header
 #define BA_header
 
+struct Info {
+	char name[CHARLIMIT], city[CHARLIMIT], address[CHARLIMIT];
+};
 
+struct type {
+
+};
 
 
 #endif
--- BankAcct-Project/BankAcct-Project/mainProgram.cpp
+++ BankAcct-Project/BankAcct-Project/mainProgram.cpp
@@ -1,15 +1,38 @@
 #include "BA_header.h"
 
 
-
-
 int main(void){
 
+	char acctId[CHARLIMIT];
+	const char* actReply[ACCTSELT];
+	int key=0; //left and right key function selection for account type
+	do {
+		system("cls");
+		printf("\n\tNo account yet? Type REGISTER to create an account.");
+		printf("\n\n\tAccount ID: ");
+		gets_s(acctId);
+		toupper(acctId[CHARLIMIT]);
+	} while (strcmp(acctId, "REGISTER") != 0);
+	
+	printf("\n\t==========	Welcome!	==========");
+	printf("\n\n\tSelect the following types:\n");
+	strcpy(actReply[0],"[TIME]");
+	do {
+		switch (key) {
+		case LTARROW:
+			printf("\033[0;33m");
+			printf("%s",);
+			printf("\033[0m");
+			printf("    [TIME]");
+			printf("    [TRUST]");
+			printf("    [CANCEL]");
+			break;
+		case RTARROW:
 
+			break;
+		}
+	}
 
-
-
-
-getch();
+_getch();
 return 0;
 }
