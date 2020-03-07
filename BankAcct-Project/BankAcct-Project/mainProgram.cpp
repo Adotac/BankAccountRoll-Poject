b0d5643a0cb4d34f71bb10e799cf8a8988c6ec3b
Adoian <otacool89@gmail.com>
2020-03-08 00:10:15 +0800

    Auto stash before merge of "jmontVersion" and "origin/playground"

--- BankAcct-Project/BankAcct-Project/mainProgram.cpp
+++ BankAcct-Project/BankAcct-Project/mainProgram.cpp
@@ -2,7 +2,12 @@
 
 
 int main(void){
+	acctDet *account;
+	account = (acctDet*)calloc(sizeof(acctDet), PLIMIT);
 
+	account[0].balance_history[0] = 50000;
+	strcpy(account[0].branch[0], "basak");
+	strcpy(account[0].date[0], __DATE__);
 	char acctId[CHARLIMIT];
 	int i=0;// IDENTIFIES THE TYPE OF SAVINGS
 
@@ -14,9 +19,52 @@ int main(void){
 		toupper(acctId[CHARLIMIT]);
 	} while (strcmp(acctId, "REGISTER") != 0);
 	
+	
+	acctHistory(account, 0);
 
+	getch();
+	return 0;
+}
 	i = accountTypeReg(i);
 
-_getch();
-return 0;
-}
+//int main(void){
+//	acctDet account[PLIMIT];
+//
+//
+//	int flag = TRUE;
+//	char acctId[CHARLIMIT];
+//	char *actReply;
+//	actReply = (char*)calloc(sizeof(char), CHARLIMIT);
+//
+//	int key=0; //left and right key function selection for account type
+//	do{
+//		system("cls");
+//		printf("\n\tNo account yet? Type REGISTER to create an account.");
+//		printf("\n\n\tAccount ID: ");
+//		gets_s(acctId);
+//		toupper(acctId[CHARLIMIT]);
+//	}while (strcmp(acctId, "REGISTER") != 0);
+//	
+//	printf("\n\t==========	Welcome!	==========");
+//	printf("\n\n\tSelect the following types:\n");
+//	strcpy(actReply,"[TIME]");
+//	
+//	do {
+//		switch (key) {
+//		case LTARROW:
+//			printf("\033[0;33m");
+//			printf("%s", "test");
+//			printf("\033[0m");
+//			printf("    [TIME]");
+//			printf("    [TRUST]");
+//			printf("    [CANCEL]");
+//			break;
+//		case RTARROW:
+//
+//			break;
+//		}
+//	}while(flag == TRUE);
+//
+//getch();
+//return 0;
+//}
