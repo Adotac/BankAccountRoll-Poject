dc33323b51cc035e0e8c83a6c6346c089859ed7f
Adoian <otacool89@gmail.com>
2020-03-16 14:07:33 +0800

    Functions being arranged
    tagsa tagsa na ang cpp sa types

--- BankAcct-Project/BankAcct-Project/BA_header.h
+++ BankAcct-Project/BankAcct-Project/BA_header.h
@@ -7,31 +7,23 @@
 
 #define TRUE 0
 #define FALSE 1
-#define NA -1
 
 #define CHARLIMIT 50
 #define PLIMIT 20
-#define IDLIMIT 6
 
-//ASCII codes
+#define ROWCOL 10 //account type 
+
+#define ACCTSELT 3
 #define LTARROW 75
 #define RTARROW 77
-#define ENTER 13
-
-#define INTEREST 0.03
-#define TAX 0.8
-#define DEPOSIT 150000
+#define ENTER 10
 
 #ifndef BA_header
 #define BA_header
 
-const char currentBranch[] = {"Basak"};
-const char charGenerator[] = {"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
 typedef enum Account_Type accType;
 enum Account_Type {SAVINGS, TIME, TRUST};
 
-typedef enum Trust_Benefits Tbenefits;
-enum Trust_Benefits {Null, SUCCESSOR, SCHOLARSHIP, RETIREMENT_PLAN};
 
 typedef struct NAME name;
 struct NAME {
@@ -43,97 +35,28 @@ struct NAME {
 typedef struct Info info;
 struct Info {
 	name pname;
-	int age;
 	char city[CHARLIMIT], address[CHARLIMIT];
-};
-
-typedef struct TrustDetails Trust;
-struct TrustDetails{
-
-	Tbenefits benefitType;
-	char benefit[CHARLIMIT];
-	Info beneficiaryInfo;
-
-	char assets[PLIMIT][CHARLIMIT];
-	char assetHistDate[PLIMIT][CHARLIMIT];
-	float assetAmount;
-	float assetAmtHistory[PLIMIT];
-
-	char liabilities[PLIMIT][CHARLIMIT];
-	char liabHistDate[PLIMIT][CHARLIMIT];
-	float liabAmount;
-	float liabAmtHistory[PLIMIT];
-
-	char linkedBankAcct[IDLIMIT]; // Time||savings account
-};
-
-typedef struct SavingsDetails Savings;
-struct SavingsDetails{
-	float balance_history[PLIMIT];
-	char branch[PLIMIT][CHARLIMIT];
-	char date[PLIMIT][CHARLIMIT];
 };
 
 typedef struct AccountDetails acctDet;
 struct AccountDetails{
 	accType type;
 	info inf;
-	char acctID[IDLIMIT];
+	char acctID[CHARLIMIT];
 	int pin;
+
+	float balance_history[PLIMIT];
+	char branch[PLIMIT][CHARLIMIT];
+	char date[PLIMIT][CHARLIMIT];
+
 	float totalBalance;
 
-	//IF TYPE IS X ->
-	//X = savings account
-	Savings savingType;
-	//X = Time account
-	Trust trustType;
+	
 };
+int accountTypeReg(int);
 
-void textHighllght(char[]);
-void upperSentence(char[]);
-void lowerSentence(char[]);
-int accountTypeReg();
+void acctHistory(acctDet*, int);
 
 
-//savings account
-int SavingMoneyOpts();
-void SavingsAcctHistory(acctDet*, int);
-void displaySavings(acctDet*, char[]);
-void SavingsDeposit(acctDet*, int);
-void SavingsWithdraw(acctDet*, int);
-void MoneyTransfer(acctDet*, int);
-
-//trust account
-void createTrust(acctDet*, int);
-void displayTrust(acctDet*, char[]);
-void AddLedger(acctDet *aD, int index);
-void DeleteLedger(acctDet *aD, int index);
-void LinkAccount(acctDet *aD, int index);
-
-
-
-//general functions
-int IdChecker(char[], acctDet*);
-void acctGenerator(acctDet*);
-int getAcctIndex(acctDet*, char[]);
-int pinGenerator();
-int pinChecker(char[], int, acctDet*);
-int scanIfDigits(char[]);
-int optLedger();
-int optBenefits();
-int optYesNo();
-
-//Time deposit
-void calculateTimeDeposit();
-void showMoney(float *principalMoney);
-void checkMoney(float *principalMoney);
-void depositMoney(float *principalMoney);
-void withrawMoney(float *principalMoney);
-void TimeDeposit(acctDet *aD, int index);
-
-void deleteAccount(acctDet*, int);
-
-
-void getInfo(acctDet*, int);
 
 #endif
--- BankAcct-Project/BankAcct-Project/mainProgram.cpp
+++ BankAcct-Project/BankAcct-Project/mainProgram.cpp
@@ -2,87 +2,21 @@
 
 
 int main(void){
-	srand(time(NULL));
 
-	acctDet *account;
-	account = (acctDet*)calloc(sizeof(acctDet), PLIMIT);
-	//*account = InitializeAcct();
+	char acctId[CHARLIMIT];
+	int i=0;// IDENTIFIES THE TYPE OF SAVINGS
 
-	int loginFlag = TRUE, pinInput = NULL, a_index = NULL;
-	
-	char *inputId;
-	inputId = (char*)calloc(sizeof(char), CHARLIMIT);
-	
-	//temporary account dont delete
-	strcpy(account[0].acctID, "ADMIN");
-	account[0].type = TIME;
-	account[0].savingType.balance_history[0] = 50000;
-	strcpy(account[0].savingType.branch[0], "basak");
-	strcpy(account[0].savingType.date[0], __DATE__);
-	
-
-	do {//main loop
-		fflush(stdin);
+	do {
 		system("cls");
-		
 		printf("\n\tNo account yet? Type REGISTER to create an account.");
 		printf("\n\n\tAccount ID: ");
-		gets(inputId);
-		upperSentence(inputId);
-
-		if( strcmp(inputId, "REGISTER") == 0){	
-			acctGenerator(account);	
-			//dire ang flow sa 1-4
-		}
-		else if(IdChecker(inputId, account) == TRUE){
-			fflush(stdin);
-			printf("\n\tPIN: ");
-			scanf(" %d", &pinInput);
-			//ari na ang pin input
-			if(pinChecker(inputId, pinInput, account) == TRUE){
-				//ari na sulod tanan after login
-				a_index = getAcctIndex(account, inputId);
-				switch(account[a_index].type){
-				case TIME:
-					TimeDeposit(account, a_index);
-					break;
-				case SAVINGS:
-					displaySavings(account, inputId);
-					break;
-				case TRUST:
-					displayTrust(account, inputId);
-					break;
-				}
-			}
-			else{
-				printf("\nACCOUNT ID or PIN doesn't match");
-				getch();
-				continue;
-			}
-			
-		
-
-			
-		}
-		else if(strcmp(inputId, "EXIT") == 0){
-			loginFlag = FALSE;
-		}
-		else{ 
-			printf("\n:::::Account doesn't exist!!:::::");
-			getch();
-		}
-
-		
-
-	}while(loginFlag == TRUE);
-	
-	
-	
+		gets_s(acctId);
+		toupper(acctId[CHARLIMIT]);
+	} while (strcmp(acctId, "REGISTER") != 0);
 	
 
-	free(account);
-	free(inputId);
-	return 0;
+	i = accountTypeReg(i);
+
+_getch();
+return 0;
 }
-	
-
